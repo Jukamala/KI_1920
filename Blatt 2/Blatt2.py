6ba6cf9fab4091c2c28e63de275289fb86dcfa64
Justus <justuswill@gmail.com>
2019-12-04 18:10:30 +0100

    overview

--- Blatt 2/Blatt2.py
+++ Blatt 2/Blatt2.py
@@ -21,18 +21,31 @@ def plot_data(x_train, y_train, x_test, y_test):
 
 
 # plot data + predicted and real labels
+# returns predictions
 def plot_predict(model, x, y):
     fig, ax = plt.subplots(figsize=(10, 5))
     y_predict = model.predict(x)
     dist = np.abs(y - y_predict)
     norm = plt.Normalize(np.min(dist), np.max(dist))
     ax.scatter(x, y, s=4, c=plt.cm.viridis_r(norm(dist)))
-    ax.plot(x, y_predict, c='red', label = 'y_predict [ED = %.2f]' % ed(y, y_predict))
+    ax.plot(x, y_predict, c='red', label='y_predict [ED = %.2f]' % ed(y, y_predict))
     ax.legend(loc='lower left')
     ax.set_title(r'Model: $%s$' % ''.join(['+ %.2f x^{%d}' % (a, i) if i > 0 and a >= 0 else
                   '%.2f x^{%d}' % (a, i) for i, a in enumerate(model.model)]).replace('x^{0}','').replace('x^{1}','x'))
     cax = make_axes_locatable(ax).append_axes('right', size='2%', pad=0.05)
     plt.colorbar(plt.cm.ScalarMappable(norm=norm, cmap='viridis_r'), cax=cax).set_label('distance to prediction')
+    plt.show()
+    return y_predict
+
+
+#Combine all predictions in a nice plot
+def overview(x_train, y_train, x_test, y_test, predictions):
+    fig, ax = plt.subplots(figsize=(10, 5))
+    ax.scatter(x_train, y_train, s=4, c='darkcyan', label='train_data')
+    ax.scatter(x_test, y_test, s=4, c='turquoise', label='test_data')
+    for c, [i, p] in zip(['red', 'orangered', 'darkorange'], enumerate(predictions)):
+        ax.plot(x_test, p, c=c, label='Prediction with degree %d [ED = %.2f]' % (i+2, ed(y_test, p)))
+    ax.legend(loc='lower left')
     plt.show()
 
 
@@ -70,11 +83,13 @@ if __name__ == '__main__':
     [x_train, y_train], [x_test, y_test] = data_loader()
     plot_data(x_train, y_train, x_test, y_test)
 
+    predictions = []
     for p in range(2, 5):
         model = RegressionModel(p)
         model.fit(x_train, y_train)
         plot_predict(model, x_train, y_train)
-        plot_predict(model, x_test, y_test)
+        predictions += [plot_predict(model, x_test, y_test)]
+    overview(x_train, y_train ,x_test, y_test, predictions)
 
     y = np.array([0.8, 0.43, 1.74, 0.26, 4.06, 0.73, 2.8, 3.37])
     y_hat = np.array([3.49, 1.3, 1.49, 4.12, 2.19, 4.24, 4.67, 0.22])
