135f420f403bc081fce030f6e77746850c8a2032
Justus Will <justuswill@gmail.com>
2019-12-04 18:10:49 +0100

    Gradient Descent

--- Blatt 2/Blatt2.py
+++ Blatt 2/Blatt2.py
@@ -6,7 +6,7 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 # Load data from csv
 def data_loader():
-    return [np.transpose(pd.read_csv('%s_Dataset.csv' % f, sep=' ', header=None).to_numpy()) for f in ['Train', 'Test']]
+    return [pd.read_csv('%s_Dataset.csv' % f, sep=' ', header=None).to_numpy().T for f in ['Train', 'Test']]
 
 
 # Plot all train and test data
@@ -40,18 +40,54 @@ def plot_predict(model, x, y):
 def ed(y, y_hat):
     return np.sqrt(np.mean((y - y_hat)**2))
 
+#Mean Square Error
+def mse(y, y_hat):
+    return np.mean((y - y_hat)**2)
+
 
 class RegressionModel:
     def __init__(self, degree):
         self.degree = degree
         self.model = None
+        self.Normalizers = [plt.Normalize(), plt.Normalize()]
 
-    def fit(self, x_train, y_train):
-        # Normalengleichung
+    def fit(self, x_train, y_train, mu=1, tol=0.00001):
+        #Normalize data
+        self.Normalizers[0].autoscale(x_train)
+        self.Normalizers[1].autoscale(y_train)
+
+        #Normalize
+        x_train = np.array(self.Normalizers[0].__call__(x_train))
+        y_train = np.array(self.Normalizers[1].__call__(y_train))
+
+        #Features
         X = self._generate_features(x_train)
-        self.model = np.linalg.solve(np.dot(X.T, X), np.dot(X.T, y_train))
+
+        # Normalengleichung as a starting point
+        try:
+            self.model = np.linalg.solve(np.dot(X.T, X), np.dot(X.T, y_train))
+        except np.linalg.LinAlgError:
+            # Start Gaussian distribiution
+            self.model = np.random.normal(0, 1, self.degree + 1)
+
+        # Fine-Tune this with the Gradient Descent to combat rounding errors
+        gradient = np.matmul(X.T, np.matmul(X, self.model)) - np.matmul(y_train.T, X)
+        steps = 0
+        while np.linalg.norm(gradient) > tol:
+            gradient = np.matmul(X.T, np.matmul(X, self.model)) - np.matmul(y_train.T, X)
+
+            #Get a status avery 1.000 Steps
+            steps += 1
+            if steps == 1000:
+                steps = 0
+                print("Norm: %.4f\nMSE: %.4f" % (np.linalg.norm(gradient), mse(self.predict(x_train), y_train)))
+
+            self.model = self.model - mu * (1/len(x_train)) * gradient
 
     def predict(self, x_test):
+        #Normalized on x_train
+        x_test = np.array(self.Normalizers[0].__call__(x_test))
+
         if self.model is None:
             raise RuntimeError('fit a model using .fit(...) before calling .predict(...)')
 
@@ -59,11 +95,14 @@ class RegressionModel:
         res = 0
         for a in np.flip(self.model):
             res = x_test * res + a
-        return res
+
+        #De-normalize
+        return np.array(self.Normalizers[1].inverse(res))
 
     def _generate_features(self, x_values):
         p, x_val = np.meshgrid(np.arange(0, self.degree + 1), x_values)
         return x_val**p
+
 
 
 if __name__ == '__main__':
